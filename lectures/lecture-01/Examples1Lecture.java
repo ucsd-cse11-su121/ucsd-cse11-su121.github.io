db747b7cbe8bcfa048088d234d5a4774c18908d0
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-06-29 12:44:20 -0700

    Lecture 01 updates

--- lectures/lecture-01/Examples1Lecture.java
+++ lectures/lecture-01/Examples1Lecture.java
@@ -1,3 +1,12 @@
 class Examples1Lecture {
-  int theNumberFive = 2 + 3;
+  //int theNumberFive = 2 + 3;
+  int x = 2 + 9 * 3;
+  int y1 = 10 / 3;
+  int y2 = 11 / 3;
+
+  int x2 = 2 + (9 / 3) * 3;
+  int x3 = (2 + 9) * 3;
+
+  //int z = 5 * 3;
+  //int somethingElse = 5 - 3;
 }
---
+++ lectures/lecture-01/Examples2.java
@@ -0,0 +1,10 @@
+class Examples2 {
+    int hourlyRate = 25;
+    int hoursWorked = 33;
+    int calculatedDireclty = 20 * 33;
+    int calculatedFromFields = this.hourlyRate * this.hoursWorked;
+
+    int week1Pay = this.hourlyRate * 33;
+    int week2Pay = this.hourlyRate * 40;
+    int week3Pay = this.hourlyRate * 22;
+}
