266b6a3d95a9d9c62c1938dc9b757f4322ec900f
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-07-14 12:27:33 -0700

    Lecture 11 updates

--- lectures/lecture-11/LoopsExamples.java
+++ lectures/lecture-11/LoopsExamples.java
@@ -57,6 +57,10 @@ class LoopsExamples {
         double total = 0.0;
         //int count = 0;
 
+        if (doubles.length == 0) {
+            return 0.0;
+        }
+
         for (double d: doubles) {
             total = total + d;
             //count = count + 1;
@@ -68,9 +72,66 @@ class LoopsExamples {
 
     double[] ds1 = { 2.0, 4.0, 1.0 };       //  7 / 3
     double[] ds2 = { 5.0, 1.0, 3.0, 6.0 };  // 15 / 4
+    double[] ds3 = { };                     //  0 / 0
 
     boolean testAverage(Tester t) {
         return t.checkExpect(this.average(ds1), 7.0 / 3.0) &&
-               t.checkExpect(this.average(ds2), 15.0 / 4.0);
+               t.checkExpect(this.average(ds2), 15.0 / 4.0) &&
+               t.checkExpect(this.average(ds3), 0.0);
+    }
+
+    //write sumEvens - try writing sumEvens, which takes an array of ints and
+    //returns the sum of just the even ones
+    //{ 1,  2, 7, 4 }    -> 6
+    //{ 4,  2, 4, 5 }    -> 10
+    //{ 0, -2, 7, 4, 8 } -> 10
+    int sumEvens(int[] array) {
+        int sum = 0;
+
+        for (int i: array) {
+            if (i % 2 == 0) {
+                //System.out.println("i: " + i + ", sum: " + sum);  //debug statements
+                sum += i;               //sum = sum + i;
+            }
+        }
+
+        return sum;
+    }
+
+    int[] array1 = { 1, 2, 7, 4 };      //  6
+    int[] array2 = { 4, 2, 4, 5 };      //  10
+    int[] array3 = { 0, -2, 7, 4, 8 };  //  10
+    int[] array4 = { 1, 2, 4, 5 };      //  6
+    int[] array5 = { 1, 2, 4, 5, 100 }; //  106    //105
+
+    boolean testSumEvens(Tester t) {
+        System.out.println("sumEvens: array1: " + this.sumEvens(array1));
+        System.out.println("sumEvens: array2: " + this.sumEvens(array2));
+        System.out.println("sumEvens: array3: " + this.sumEvens(array3));
+
+        return t.checkExpect(this.sumEvens(array1), 6) &&
+               t.checkExpect(this.sumEvens(array2), 10) &&
+               t.checkExpect(this.sumEvens(array3), 10) &&
+               t.checkExpect(this.sumEvens(array4), 6);
+    }
+
+    //sumEveryOther - which adds up the elements at the even indices
+    //in an array of int and return the result
+    int sumEveryOther(int[] nums) {
+        int total = 0;
+
+        for (int index = 0; index < nums.length; index += 2) {
+            total += nums[index];
+        }
+
+        return total;
+    }
+
+    void testEveryOther(Tester t) {
+        t.checkExpect(this.sumEveryOther(array1), 8);
+        t.checkExpect(this.sumEveryOther(array2), 8);
+        t.checkExpect(this.sumEveryOther(array3), 15);
+        t.checkExpect(this.sumEveryOther(array4), 5);
+        t.checkExpect(this.sumEveryOther(array5), 105);
     }
 }
--- lectures/lecture-11/LoopsExamples2.java
+++ lectures/lecture-11/LoopsExamples2.java
@@ -1,5 +1,27 @@
 //import tester.*;
 
 class LoopsExamples2 {
-    
+    public static void main(String[] args) {
+
+        //   init         test     update
+        for (int num = 0; num < 7; num = num + 1) {
+            //loop body
+            System.out.println("num is: " + num);
+        }
+    }
+
+
+    //factorial: takes a number n and returns n!
+    int factorial(int n) {
+        int result = 0;
+
+        for (int current = 0; current < n; current = current + 1) {
+            result = result * current;
+        }
+
+        return result;
+    }
+
+    int result1 = factorial(4); //should be 24
+    int result2 = factorial(5); //should be 120
 }
