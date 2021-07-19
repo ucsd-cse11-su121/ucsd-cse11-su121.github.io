93ed360cf5cb317a43798e4717790528eaf6f192
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-07-19 10:52:42 -0700

    Lecture 13 updates

--- lectures/lecture-13/PointExamples.java
+++ lectures/lecture-13/PointExamples.java
@@ -21,8 +21,45 @@ class Point {
   // the point in the array pts that is the *closest* (according to distance) to
   // this Point
 
+  //Write a method allWithin that takes an array of Points and an integer
+  //dist and returns a new array of all points within dist of this point
+  //from that array
+  Point[] allWithin(Point[] pts, int dist) {
+    int numberWithin = 0;
+    for (Point p: pts) {
+      if (this.distance(p) < dist) {
+        numberWithin += 1;
+      }
+    }
+
+    Point[] toReturn = new Point[numberWithin];
+
+    int index = 0;
+    for (Point p: pts) {
+      if (this.distance(p) < dist) {
+        toReturn[index] = p;    //array update expressions
+        index += 1;
+      }
+    }
+
+    return toReturn;
+  }
 }
 
 class PointExamples {
+  Point p1 = new Point(4, 5);
+  Point p2 = new Point(10, 10);
+  Point p3 = new Point(100, 100);
+  Point p4 = new Point(0, 0);
 
+  Point[] allButP1 = { p2, p3, p4 };
+  Point[] allButP1P3 = { p2, p4 };
+  Point[] empty = { };
+
+  Point[] result1 = p1.allWithin(allButP1, 20);
+
+  void testAllWitthin(Tester t) {
+    t.checkExpect(p1.allWithin(allButP1, 20), allButP1P3);
+    t.checkExpect(p1.allWithin(allButP1, 3), empty);
+  }
 }
