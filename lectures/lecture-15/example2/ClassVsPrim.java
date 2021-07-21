75317b587de3b5a5d376ad6dea307f809aa10a7a
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-07-21 10:52:15 -0700

    Lecture 15 updates

--- lectures/lecture-15/example2/ClassVsPrim.java
+++ lectures/lecture-15/example2/ClassVsPrim.java
@@ -18,4 +18,16 @@ class Point {
 
 public class ClassVsPrim {
 
+  //Pair<int, int> p = new Pair<>(56, 55);
+
+  //Integer i = new Integer(45);  //old way, don't use 
+  // This mens the same thing as above
+  Integer i = 45;                 //use this
+
+  Integer i2 = i + 3;
+
+  int n = i2;
+  int n2 = i2.intValue();
+
+  Pair<Integer, Boolean> p = new Pair<>(45, true);
 }
