8e7f7db290fa05e7c6176d5cade1f32e62721dc6
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-07-27 13:24:02 -0700

    Lecture 18 updates

--- lectures/lecture-18/Equality.java
+++ lectures/lecture-18/Equality.java
@@ -15,16 +15,34 @@ class Point {
     boolean same(Point other) {
         return (this.x == other.x) && (this.y == other.y);
     }
+
+    //This is an OVERLOADED equals method, the one in Object class
+    //has a different signature: boolean equals(Object other)
+    boolean equals(Point other) {
+        //System.out.println("Calling our equals method now!");
+        return (this.x == other.x) && (this.y == other.y);
+    }
+
+    //Implementation of the equals method that is defined in Object class
+    //This method OVERRIDEs the equals method in the Object class
+    public boolean equals(Object other) {
+        System.out.println("Calling our equals method now!");
+
+        if (other == null) {
+            return false;
+        }
+
+        if (!(other instanceof Point)) {
+            return false;
+        }
+
+        // other must be a Point here (or a class that extends Point)
+        Point p = (Point) other;
+        return (this.x == p.x) && (this.y == p.y);
+    }
 }
 
 class Equality {
-
-    Point p = new Point(4, 5);
-    Point p2 = new Point(4, 5);
-
-    boolean pAndP2EqualsByOperator = (p == p2);
-    boolean pAndP2Equals = p.equals(p2);    //Calling the .equals method on built-in Object class
-    boolean pAndP2SameByMethod = p.same(p2);
 
     int[] numsA = { 5, 1, 7 };
     int[] numsB = { 5, 1, 7 };
@@ -40,10 +58,39 @@ class Equality {
     String[] strs1 = { s1, s2 };
     String[] strs2 = { s2, s1 };
 
+    boolean str1EqualsStr2 = s1.equals(s2);
     boolean strsEqualsByOperator = (strs1 == strs2);
     boolean strsEqualByMethod = strs1.equals(strs2);
     boolean strsEqualByArrayEqual = Arrays.equals(strs1, strs2);
     boolean s1AndS2EqualByOperator = (s1 == s2);
+
+
+    Point p1 = new Point(4, 5);
+    Point p2 = new Point(4, 5);
+
+    boolean pAndP2EqualsByOperator = (p1 == p2);
+    boolean pAndP2Equals = p1.equals(p2);    //Calling the .equals method on built-in Object class
+    boolean pAndP2SameByMethod = p1.same(p2);
+
+    Point[] points1 = { p1, p2 };
+    Point[] points2 = { p2, p1 };
+
+    boolean pointsEqualByArraysEquals = Arrays.equals(points1, points2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         String appleVar1 = "apple";
--- lectures/lecture-18/JavaFun.java
+++ lectures/lecture-18/JavaFun.java
@@ -1,7 +1,39 @@
+class A { } 
+class B extends A { }
+class C extends A { }
+
 class JavaFun {
+
+    C c = new C();
+    B b = new B();
+    A a = new A();
+    A aTypedVariableHoldingAC = new C();
+
+    boolean aInstanceOfA = a instanceof A;
+    boolean bInstanceOfB = b instanceof B;
+    boolean cInstanceOfC = c instanceof C;
+
+    boolean cInstanceOfA = c instanceof A;
+
+    //Java won't let us run the next line
+    //boolean cInstanceOfB = c instanceof B;
+
+    boolean aInstanceOfC = a instanceof C;
+    boolean cInAVarInstanceOfC = aTypedVariableHoldingAC instanceof C;
+    boolean cInstanceOfObject = c instanceof Object;
+
+    C canWeCopyCInAVarHere = (C) aTypedVariableHoldingAC;
+    C canWeCopyAHere = (C) a;
+
+
+
+
+
+
+
     //int x = 234234234;
     //int x = 2100000000;
-
+/*
     int x = 2147483647;
     int y = -2147483648;
 
@@ -18,4 +50,5 @@ class JavaFun {
     //char c = 'asfasdf';   //error
 
     String s = c + "bc";
+*/    
 }
