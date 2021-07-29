ca8ce4c4fd61d100cc8ba5321a20956b512fd66c
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2021-07-29 12:24:36 -0700

    Lecture 20 updates

--- lectures/lecture-20/filesystem/FilesystemExamples.java
+++ lectures/lecture-20/filesystem/FilesystemExamples.java
@@ -1,8 +1,23 @@
 import tester.*;
 import java.util.List;
 
-interface FolderContent {
+ // Challenge: implement and test countFiles, which takes no arguments and
+  // returns the total number of File objects contained within a Folder1/Folder2
+  // and all of its subfolders
 
+  // Challenge: implement and test containsString, which takes a string to
+  // search for in a directory and returns true if any file found in that
+  // directory contains a file whose contents contains that string
+
+  // Challenge: implement and test totalSize, which takes no arguments and
+  // returns the total count of characters in all the files in that directory
+  // structure.
+
+interface FolderContent {
+    boolean containsFile(String name);
+    public int countFiles();
+    public boolean containsString(String str);
+    public int totalSize();    
 }
 
 class File implements FolderContent {
@@ -12,6 +27,24 @@ class File implements FolderContent {
         this.name = name;
         this.contents = contents;
     }
+
+    public boolean containsFile(String name) {
+        return this.name.equals(name);
+    }
+
+    public int countFiles() {
+        return 1;
+    }
+
+    public boolean containsString(String str) {
+        //System.out.println("file containsString: " + str);
+        return this.contents.contains(str);
+    }
+
+    public int totalSize() {
+        //System.out.println("file totalSize: " + this.contents.length());
+        return this.contents.length();
+    }
 }
 
 class Folder1 implements FolderContent {
@@ -20,6 +53,46 @@ class Folder1 implements FolderContent {
     Folder1(String name, List<FolderContent> contents) {
         this.name = name;
         this.contents = contents;
+    }
+
+    public boolean containsFile(String name) {
+        for (FolderContent fc: this.contents) {
+            if (fc.containsFile(name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int countFiles() {
+        int count = 0;
+
+        for (FolderContent fc: this.contents) {
+            count += fc.countFiles();
+        }
+
+        return count;
+    }
+
+    public boolean containsString(String str) {
+        for (FolderContent fc: this.contents) {
+            if (fc.containsString(str)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int totalSize() {
+        int count = 0;
+
+        for (FolderContent fc: this.contents) {
+            count += fc.totalSize();
+        }
+
+        return count;
     }
 }
 
@@ -31,6 +104,62 @@ class Folder2 {
         this.name = name;
         this.files = files;
         this.folders = folders;
+    }
+
+    public boolean containsFile(String name) {
+        for (File f: this.files) {
+            if (f.containsFile(name)) {
+                return true;
+            }
+        }
+
+        for (Folder2 f: this.folders) {
+            if (f.containsFile(name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int countFiles() {
+        int count = files.size();
+
+        for (Folder2 f: this.folders) {
+            count += f.countFiles();
+        }
+
+        return count;
+    }
+
+    public boolean containsString(String str) {
+        for (File f: this.files) {
+            if (f.containsString(str)) {
+                return true;
+            }
+        }
+
+        for (Folder2 f: this.folders) {
+            if (f.containsString(str)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int totalSize() {
+        int count = 0;
+
+        for (File f: this.files) {
+            count += f.totalSize();
+        }
+
+        for (Folder2 f: this.folders) {
+            count += f.totalSize();
+        }
+
+        return count;
     }
 }
 
@@ -44,7 +173,7 @@ class FilesystemExamples {
     );
 
     Folder1 lecture19f1 = new Folder1(
-        "lecture-19",
+        "filesystem",
         List.of(new File("FilesystemExamples.java", "<java code>"), textfiles)
     );
 
@@ -58,8 +187,41 @@ class FilesystemExamples {
     );
 
     Folder2 lecture19f2 = new Folder2(
-        "lecture-19",
+        "filesystem",
         List.of(new File("FilesystemExamples.java", "<java code>")),
         List.of(textfiles2)
     );
+
+    void testContainsFile(Tester t) {
+        t.checkExpect(lecture19f1.containsFile("haiku.txt"), true);
+        t.checkExpect(lecture19f1.containsFile("notthere.txt"), false);
+        t.checkExpect(dictionaries.containsFile("haiku.txt"), false);
+        t.checkExpect(textfiles.containsFile("FilesystemExamples.java"), false);
+
+        t.checkExpect(lecture19f2.containsFile("haiku.txt"), true);
+        t.checkExpect(lecture19f2.containsFile("notthere.txt"), false);
+        t.checkExpect(dictionaries2.containsFile("haiku.txt"), false);
+        t.checkExpect(textfiles2.containsFile("FilesystemExamples.java"), false);
+    }
+
+    void testCountFiles(Tester t) {
+        t.checkExpect(lecture19f1.countFiles(), 3);
+        t.checkExpect(lecture19f2.countFiles(), 3);
+    }
+
+    void testContainsString(Tester t) {
+        t.checkExpect(lecture19f1.containsString("cat"), true);
+        t.checkExpect(lecture19f1.containsString("notext"), false);
+
+        t.checkExpect(lecture19f2.containsString("cat"), true);
+        t.checkExpect(lecture19f2.containsString("notext"), false);
+    }
+
+    void testTotalSize(Tester t) {
+        System.out.println("Folder1 totalSize=" + lecture19f1.totalSize());
+        System.out.println("Folder2 totalSize=" + lecture19f2.totalSize());
+
+        t.checkExpect(lecture19f1.totalSize(), 30);
+        t.checkExpect(lecture19f2.totalSize(), 30);
+    }
 }
